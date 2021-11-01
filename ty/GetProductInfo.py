8cc8102a2a665550bfa051a37138f6571c43b1be
hasanyucel <hasanyucel34@gmail.com>
2021-11-01 10:47:56 +0300

    GetProductInfo düzeltmeler yapıldı.

--- ty/GetProductInfo.py
+++ ty/GetProductInfo.py
@@ -22,97 +22,30 @@ class GetProductInfo:
     def getProductID(self):
         return self.data["product"]["id"]
 
-    def saveImages(self):
-        images = ['https://cdn.dsmcdn.com/' + img for img in self.data['product']['images']]
-        if not os.path.exists('images/'+str(self.data["product"]["id"])+''):
-            os.makedirs('images/'+str(self.data["product"]["id"])+'')
-        i = 1
-        for image in images:
-            response = requests.get(image)
-            file = open('images/'+str(self.data["product"]["id"])+'/'+str(i)+'.jpg', 'wb')
-            file.write(response.content)
-            file.close()
-            i = i + 1
-
-    def getProductName(self):#
+    def getProductName(self):
         return self.data["product"]["name"]
-
-    def getProductOrginalPrice(self):
-        return self.data["product"]["price"]["originalPrice"]["value"]
-
-    def getProductSellingPrice(self):
-        return self.data["product"]["price"]["sellingPrice"]["value"]
-    
-    def getProductDiscountedPrice(self):
-        return self.data["product"]["price"]["discountedPrice"]["value"]
-
-    def getProductBrand(self):
-        return self.data["product"]["metaBrand"]["name"]
-
-    def getProductSellerName(self):
-        return self.data["product"]["merchant"]["name"]
-    
-    def getProductSellerScore(self):
-        try:
-            return self.data["product"]["merchant"]["sellerScore"]
-        except:
-            return "-"
-
-    def getProductSellerCityName(self):
-        return self.data["product"]["merchant"]["cityName"]
-
-    def getProductSellerOfficialName(self):
-        return self.data["product"]["merchant"]["officialName"]
-    
-    def getProductSellerTaxNumber(self):
-        return self.data["product"]["merchant"]["taxNumber"]
 
     def getProductURL(self):
         return 'https://www.trendyol.com'+self.data["product"]["url"]
 
-    def getProductRatingCount(self):
-        return self.data["product"]["ratingScore"]["totalRatingCount"]
-
-    def getProductAverageRating(self):
-        return self.data["product"]["ratingScore"]["averageRating"]
-
-    def getProductTotalCommentCount(self):
-        return self.data["product"]["ratingScore"]["totalCommentCount"]
-
-    def getProductFavoriteCount(self):
-        return self.data["product"]["favoriteCount"]
-
     def getAllProductData(self):
         return self.data["product"]
 
-    def getProductMerhactCount(self):
-        return len(self.data["product"]["otherMerchants"]) + 1
-
-    def getProductAllMerchantNames(self):
-        merchants = ""
-        count = len(self.data["product"]["otherMerchants"])
-        for i in range (count):
-            merchants = merchants + "," + self.data["product"]["otherMerchants"][i]["merchant"]["name"]
-        return self.data["product"]["merchant"]["name"]+merchants
-    
-    def getProductBarcode(self):
-        return self.data["product"]["variants"][0]["barcode"]
-
-    def getAttributes(self):#
+    def getAttributes(self):
         attr = self.data["product"]["attributes"]
         result = ""
         for at in attr:
             result = result + at['key']['name'] + " : " + at['value']['name'] + " | "
         return result[:-2]
 
-    def getImages(self):#
+    def getImages(self):
         images = ['https://cdn.dsmcdn.com/' + img for img in self.data['product']['images']]
         result = ""
         for image in images:
             result = result + image +  " | "
         return result[:-2]
     
-    def getDeliveryInformation(self):#
+    def getDeliveryInformation(self):
         return self.data["product"]["deliveryInformation"]["deliveryDate"]
     
     def getDetails(self):
