7777cb1ba1c100aae255ca534085fd3b1a9ff98e
hasanyucel <hasanyucel34@gmail.com>
2021-11-03 17:44:58 +0300

    getGetProductDetailInfo yazıldı.

--- ty/trendyol.py
+++ ty/trendyol.py
@@ -1,5 +1,6 @@
 import json, regex, re, cloudscraper
 from bs4 import BeautifulSoup
+import pandas as pd
 from rich import print
 
 cat_or_seller = 1 #1 Kategori 2 Satıcı
@@ -43,6 +44,28 @@ def getProductLinks(urls):
             products.append('https://www.trendyol.com'+ data["products"][i]["url"])
     return products
 
-productPageUrls = createProductPageUrls(url,urun_adedi)
-productLinks = getProductLinks(productPageUrls)
-print(productLinks)
+def getGetProductDetailInfo(url):
+    scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False},delay=200)
+    data = scraper.get(url).content
+    soup = BeautifulSoup(data, 'html.parser')
+    nf = soup.find("h1").text
+    if nf == "404":
+        data = "Not Found"
+    else:
+        products = soup.findAll('script', text = re.compile('__PRODUCT_DETAIL_APP_INITIAL_STATE__'))
+        pattern = '\{(?:[^{}]|(?R))*\}'
+        result = regex.search(pattern, str(products[0]))[0]
+        data = json.loads(result)
+    return data
+
+def createExcelProductDetails(productLinks,urun_adedi):
+    for link in productLinks:
+        productDetailData = getGetProductDetailInfo(link) # Ürün linklerindeki ürünlerin detaylarını getir.
+        if productDetailData == "Not Found":
+            continue
+
+productPageUrls = createProductPageUrls(url,urun_adedi) # Ürün sayısına göre getirelecek sayfa linklerini oluştur (ürün sayısı / 24)
+productLinks = getProductLinks(productPageUrls) # Sayfalarındaki ürün linklerini getir.
+#print(productLinks)
+createExcelProductDetails(productLinks,urun_adedi) # Ürün detaylarıyla excel oluşturur.
+#print(productDetailData)
