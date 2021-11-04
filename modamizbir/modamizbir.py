dad3ddad97f24b3ab67654e6ed89df40b47963dc
hasanyucel <hasanyucel34@gmail.com>
2021-11-04 16:25:53 +0300

    makeHyperlink eklendi.

--- modamizbir/modamizbir.py
+++ modamizbir/modamizbir.py
@@ -1,6 +1,9 @@
 import cloudscraper, json, pandas as pd
 from rich import print
 
+def makeHyperlink(url):
+    return f'=Hyperlink("{url}","Ürün")'
+
 def getAllProductData(slug):
     url = f"https://api.modamizbir.com/api/v1/product-detail?slug={slug}&lang=tr"
     scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False},delay=200)
@@ -20,12 +23,14 @@ def getSlugsFromLinks(links):
     return slugs
 
 def createExcelData(slugs):
+    df = pd.DataFrame(columns=['Link','Ürün ID','Ürün Adı','Renk','Toplam Stok','Beden','Stok','Fiyat'])
     for slug in slugs:
         productData = getAllProductData(slug)
         productLink = "https://www.modamizbir.com/"+slug
         if productData["status"] != 0:
             productId = productData["data"]["id"]
             productName = productData["data"]["UrunAdi"]
+            productName = productName.rsplit(' ', 1)[0]
             productProperties = productData["data"]["properties"]
             productColor = ""
             for prop in productProperties:
@@ -40,13 +45,28 @@ def createExcelData(slugs):
             productPrice = str(round(productPrice,2)) + " " + productData["data"]["para_birimi_sembol"]
             for option in productOptions:
                 productVariant = option["Deger"]
-                productStock = option["StokAdeti"]
+                productStock = int(option["StokAdeti"])
                 print(productLink,productId,productName,productColor,productTotalStock,productVariant,productStock,productPrice)
+                satir = {'Link':productLink,'Ürün ID':productId,'Ürün Adı':productName,'Renk':productColor,'Toplam Stok':productTotalStock,'Beden':productVariant,'Stok':productStock,'Fiyat':productPrice}
+                df = df.append(satir, ignore_index=True)
         else:
             print(productLink, " linkinde ürün bulunamadı!")
+    saveExcel(df)
 
-links = getLinksFromExcelFile("urun-kodlari")
-slugs = getSlugsFromLinks(links)
+def saveExcel(df):
+    df['Link'] = df.apply(lambda row : makeHyperlink(row['Link']), axis = 1)
+    writer = pd.ExcelWriter('stok-bilgisi.xlsx')  
+    df.to_excel(writer, sheet_name='modamizbir', index=False, na_rep='NaN')
+    for column in df:
+        column_width = max(df[column].astype(str).map(len).max(), len(column))
+        col_idx = df.columns.get_loc(column)
+        writer.sheets['modamizbir'].set_column(col_idx, col_idx, column_width+1)
+    col_idx = df.columns.get_loc('Link')
+    writer.sheets['modamizbir'].set_column(col_idx, col_idx, 10)
+    writer.save() 
+
+#links = getLinksFromExcelFile("urun-kodlari")
+#slugs = getSlugsFromLinks(links)
 #print(slugs)
-#slugs = ['mavi-likrali-bayan-askili-pijama-takimi-54623','siyah-fitilli-bayan-tayt-67170','biskuvi-fitilli-bayan-tayt-67171']
+slugs = ['mavi-likrali-bayan-askili-pijama-takimi-54623','siyah-fitilli-bayan-tayt-67170','biskuvi-fitilli-bayan-tayt-67171']
 createExcelData(slugs)
