251f955733c376260f0ad804802ce3bb3e439fac
hasanyucel <hasanyucel34@gmail.com>
2021-11-04 11:02:29 +0300

    createExcelData yazıldı.

--- modamizbir/modamizbir.py
+++ modamizbir/modamizbir.py
@@ -1,8 +1,5 @@
-import cloudscraper
-import json
+import cloudscraper, json, pandas as pd
 from rich import print
-import pandas as pd
-
 
 def getAllProductData(slug):
     url = f"https://api.modamizbir.com/api/v1/product-detail?slug={slug}&lang=tr"
@@ -22,8 +19,31 @@ def getSlugsFromLinks(links):
         slugs.append(link)
     return slugs
 
-links = getLinksFromExcelFile("urun-kodlari")
+def createExcelData(slugs):
+    for slug in slugs:
+        productData = getAllProductData(slug)
+        productLink = "https://www.modamizbir.com/"+slug
+        productId = productData["data"]["id"]
+        productName = productData["data"]["UrunAdi"]
+        productProperties = productData["data"]["properties"]
+        productColor = ""
+        for prop in productProperties:
+            if prop["ozellik_id"] == 38:
+                productColor = prop["TeknikAdi"]
+                break
+        if productColor == "":
+            productColor = "-"
+        productOptions = productData["data"]["options"]
+        productTotalStock = productData["data"]["StokAdeti"]
+        productPrice = float(productData["data"]["Fiyat"]) * (1 + (float(productData["data"]["KdvOrani"])/100))
+        productPrice = str(round(productPrice,2)) + " " + productData["data"]["para_birimi_sembol"]
+        for option in productOptions:
+            productVariant = option["Deger"]
+            productStock = option["StokAdeti"]
+            print(productLink,productId,productName,productColor,productTotalStock,productVariant,productStock,productPrice)
+
+"""links = getLinksFromExcelFile("urun-kodlari")
 slugs = getSlugsFromLinks(links)
-print(slugs)
-"""productData = getAllProductData(slug)
-print(productData)"""
+print(slugs)"""
+slugs = ['mavi-likrali-bayan-askili-pijama-takimi-54623','siyah-fitilli-bayan-tayt-67170','biskuvi-fitilli-bayan-tayt-67171']
+createExcelData(slugs)
