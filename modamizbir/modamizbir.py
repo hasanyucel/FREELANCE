a5af644fa1d2f3dde63bab2595ebf8efbf33c422
hasanyucel <hasanyucel34@gmail.com>
2021-11-04 11:20:44 +0300

    404 sorunu çözüldü.

--- modamizbir/modamizbir.py
+++ modamizbir/modamizbir.py
@@ -23,27 +23,30 @@ def createExcelData(slugs):
     for slug in slugs:
         productData = getAllProductData(slug)
         productLink = "https://www.modamizbir.com/"+slug
-        productId = productData["data"]["id"]
-        productName = productData["data"]["UrunAdi"]
-        productProperties = productData["data"]["properties"]
-        productColor = ""
-        for prop in productProperties:
-            if prop["ozellik_id"] == 38:
-                productColor = prop["TeknikAdi"]
-                break
-        if productColor == "":
-            productColor = "-"
-        productOptions = productData["data"]["options"]
-        productTotalStock = productData["data"]["StokAdeti"]
-        productPrice = float(productData["data"]["Fiyat"]) * (1 + (float(productData["data"]["KdvOrani"])/100))
-        productPrice = str(round(productPrice,2)) + " " + productData["data"]["para_birimi_sembol"]
-        for option in productOptions:
-            productVariant = option["Deger"]
-            productStock = option["StokAdeti"]
-            print(productLink,productId,productName,productColor,productTotalStock,productVariant,productStock,productPrice)
+        if productData["status"] != 0:
+            productId = productData["data"]["id"]
+            productName = productData["data"]["UrunAdi"]
+            productProperties = productData["data"]["properties"]
+            productColor = ""
+            for prop in productProperties:
+                if prop["ozellik_id"] == 38:
+                    productColor = prop["TeknikAdi"]
+                    break
+            if productColor == "":
+                productColor = "-"
+            productOptions = productData["data"]["options"]
+            productTotalStock = productData["data"]["StokAdeti"]
+            productPrice = float(productData["data"]["Fiyat"]) * (1 + (float(productData["data"]["KdvOrani"])/100))
+            productPrice = str(round(productPrice,2)) + " " + productData["data"]["para_birimi_sembol"]
+            for option in productOptions:
+                productVariant = option["Deger"]
+                productStock = option["StokAdeti"]
+                print(productLink,productId,productName,productColor,productTotalStock,productVariant,productStock,productPrice)
+        else:
+            print(productLink, " linkinde ürün bulunamadı!")
 
-"""links = getLinksFromExcelFile("urun-kodlari")
+links = getLinksFromExcelFile("urun-kodlari")
 slugs = getSlugsFromLinks(links)
-print(slugs)"""
-slugs = ['mavi-likrali-bayan-askili-pijama-takimi-54623','siyah-fitilli-bayan-tayt-67170','biskuvi-fitilli-bayan-tayt-67171']
+#print(slugs)
+#slugs = ['mavi-likrali-bayan-askili-pijama-takimi-54623','siyah-fitilli-bayan-tayt-67170','biskuvi-fitilli-bayan-tayt-67171']
 createExcelData(slugs)
