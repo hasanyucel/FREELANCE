0e11a86a50953d00b6dca2ad6b6aa1aa855cd956
hasanyucel <hasanyucel34@gmail.com>
2022-08-22 21:02:27 +0300

    Db oluşturuldu.

---
+++ camkirangaraj.com/main.py
@@ -0,0 +1,16 @@
+import requests,json,sqlite3
+from rich import print
+
+db = "camkirangaraj.sqlite"
+
+def createDbAndTables():
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS 'Urunler' ('UrunId' TEXT NOT NULL,'Link' TEXT NOT NULL,'UrunAdi' TEXT NOT NULL,'UrunKodu' TEXT NOT NULL,'Fiyat' NUMERIC NOT NULL,PRIMARY KEY('UrunId'));")
+    conn.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS 'Detaylar' ('UrunId' TEXT NOT NULL,'UrunKategorisi' TEXT NOT NULL,'UrunAciklama' TEXT NOT NULL,'Resim1' TEXT NOT NULL,'Resim2' TEXT NOT NULL,'Resim3' TEXT NOT NULL,'Resim4' TEXT NOT NULL,'Resim5' TEXT NOT NULL,'Resim6' TEXT NOT NULL,PRIMARY KEY('UrunId'));")
+    conn.commit()
+    conn.close()
+
+
+createDbAndTables()
