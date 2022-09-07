cba3fd7e315ffebce6008426c843c85834e60b94
hasanyucel <hasanyucel34@gmail.com>
2022-09-07 23:01:31 +0300

    getSitemapLinks yazıldı

--- italyaEmlak/immobiliare/main.py
+++ italyaEmlak/immobiliare/main.py
@@ -7,16 +7,36 @@ def createDbAndTables():
     conn = sqlite3.connect(db)
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS 'SiteMapLinks' ('Url' TEXT NOT NULL,PRIMARY KEY('Url'));")
+    cur.execute("CREATE TABLE IF NOT EXISTS 'SiteLinks' ('Url' TEXT NOT NULL,PRIMARY KEY('Url'));")
     conn.commit()
 
-def insertAllSitemapLinks():
+def insertAllLinks(xml,table_name):
     conn = sqlite3.connect(db)
     cur = conn.cursor()
-    xml = 'https://www.immobiliare.it/sitemap.xml'
     r = requests.get(xml)
     soup = BeautifulSoup(r.text, 'lxml')
     urls = [loc.string for loc in soup.find_all('loc')]
     for url in urls:
-        cur.execute("INSERT OR IGNORE INTO SiteMapLinks (URL) VALUES (?)",(url,))
+        cur.execute(f"INSERT OR IGNORE INTO {table_name} (URL) VALUES (?)",(url,))
     conn.commit()
     conn.close()
+
+def getSitemapLinks():
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute('SELECT Url FROM SiteMapLinks')
+    links = cur.fetchall()
+    links = [f[0] for f in links]
+    conn.close()
+    return links
+
+print("Script is working...")
+t0 = time.time()
+createDbAndTables()
+insertAllLinks('https://www.immobiliare.it/sitemap.xml','SiteMapLinks')
+urls = getSitemapLinks()
+for url in urls:
+    insertAllLinks(url,'SiteLinks')
+
+t1 = time.time()
+print(f"{t1-t0} seconds.")
