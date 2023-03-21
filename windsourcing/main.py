abd1b765d565421d746d6fa1bd86d875cb285084
hasanyucel <hasanyucel34@gmail.com>
2023-03-22 00:41:54 +0300

    ürün bilgileri çekildi.

--- windsourcing/main.py
+++ windsourcing/main.py
@@ -1,8 +1,8 @@
-import time,sqlite3,concurrent.futures
+import time,sqlite3,concurrent.futures,cloudscraper
 from bs4 import BeautifulSoup
+from rich import print
 
 db = "windsourcing.sqlite"
-
 
 def createDbAndTables():
     conn = sqlite3.connect(db)
@@ -38,7 +38,30 @@ def getSitemapLinks(table_name):
 print("Script is working...")
 t0 = time.time()
 createDbAndTables()
-insertAllLinks('sitemap-2.xml','SiteMapLinks')
+#insertAllLinks('sitemap-1.xml','SiteMapLinks')
+#insertAllLinks('sitemap-2.xml','SiteMapLinks')
+url = "https://www.windsourcing.com/en/3m-w9910-wind-tape-adhesion-promoter-473-ml-pint"
+scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False},delay=10)
+data = scraper.get(url)
+soup = BeautifulSoup(data.text, "html.parser")
+product_title = soup.find("h1", class_="product--title").text.strip()
+product_info = soup.find("ul", class_="product--base-info list--unstyled")
+product_code = product_info.find("span").text.strip()
+
+breadcrumb_list = soup.find("ul", class_="breadcrumb--list")
+categories = []
+for li in breadcrumb_list.find_all("li", {"role": "menuitem"}):
+    categories.append(li.get_text(strip=True))
+category = " > ".join(categories)
+image_span = soup.find("span", class_="image--element")
+data_img_original = image_span["data-img-original"]
+
+
+print("URL: " + url)
+print("Ürün Adı: " + product_title)
+print("Ürün Kodu: " + product_code)
+print("Ürün Kategorisi: " + category)
+print("Ürün Fotoğraf Link: " + data_img_original)
 
 
 t1 = time.time()
