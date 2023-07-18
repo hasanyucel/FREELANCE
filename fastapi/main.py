46c232129fbee0b055d270f108ab1d400c5deb4e
hasanyucel <hasanyucel34@gmail.com>
2023-07-18 21:36:08 +0300

    root ve languages methodları eklendi.

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,6 +3,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def hello_world():
-    return {"Hello": "World"}
+def bismillahirrahmanirrahim():
+    result = {
+                "KuranAPI" : "Bismillahirrahmanirrahim!",
+                "Api Dokümanı" : "/docs",
+                "Diller": "/languages"
+             }
+    return result
 
+@app.get("/languages")
+def languages():
+    result = {
+                "Almanca (Abu Rida)":"de_aburida",
+                "İngilizce (Yusuf Ali)":"en_yusufali",
+                "Fransızca (Hamidullah)":"fr_hamidullah",
+                "İtalyanca (Piccardo)":"it_piccardo",
+                "Japonca":"ja_japanese",
+                "KU (Asan)":"ku_asan",
+                "Orjinal Arapça":"quran_text",
+                "Rusça (Muntahab)":"ru_muntahab",
+                "Türkçe (Diyanet)":"tr_diyanet",
+                "Özbekçe (Sodik)":"uz_sodik"
+             }
+    return result 
